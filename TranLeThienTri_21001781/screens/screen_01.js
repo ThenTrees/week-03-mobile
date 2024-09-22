3b89449f3624a44064c3509bdbd3d989fd896a2d
TranLeThienTri <thientri.trank17@gmail.com>
2024-09-22 15:02:33 +0700

    week 04

---
+++ TranLeThienTri_21001781/screens/screen_01.js
@@ -0,0 +1,94 @@
+import {View, StyleSheet, Image, Text} from 'react-native';
+import BackgroundImage from "../assets/images/Container17.png"
+import { TouchableOpacity } from 'react-native';
+export default Screen_01 = ({navigation}) => {
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={BackgroundImage}/>
+            </View>
+            <View style={styles.body}>
+                <Text style={styles.titleText}>Boost Productivity</Text>
+                <Text style={styles.textDesc}>Simplify tasks, boost productivity</Text>
+                <TouchableOpacity style={styles.btnSignUp} onPress={()=> {
+                    navigation.navigate('signUp')
+                }}>
+                    <Text style={ styles.textBtnSign}>Sign up</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btnLogin} >
+                    <Text style={ styles.textBtnLogin}>Login</Text>
+                </TouchableOpacity>
+                <View style={styles.listAround}>
+                    <Text style={[styles.around,styles.child]}></Text>
+                    <Text style={styles.around}></Text>
+                    <Text style={[styles.around, styles.child]}></Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginHorizontal: 18,
+        marginVertical:5,
+    },
+    header:{
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    body:{
+        gap: 15
+    },
+    titleText:{
+        textTransform: 'uppercase',
+        color: '#000',
+        fontSize: 24,
+        fontWeight: 700,
+        marginTop: 20
+        },
+    textDesc: { 
+        fontSize: 16
+    },
+    btnSignUp:{
+        backgroundColor: '#00CCFF',
+        paddingVertical: 10,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+    btnLogin:{
+        backgroundColor: '#fff',
+        paddingVertical: 10,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+    textBtnSign: {
+        fontSize: 18,
+        fontWeight: 700,
+        color: '#fff'
+    },
+    textBtnLogin:{
+        fontSize: 18,
+        fontWeight: 700,
+        color: '#000'
+    },
+    around:{
+        backgroundColor: "#00CCFF",
+        width: 20,
+        height:20,
+        borderRadius: 50,
+        marginHorizontal: 5
+    },
+    listAround:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems:'center'
+    },
+    child: {
+        backgroundColor: '#ccc'
+    }
+})
---
+++ TranLeThienTri_21001781/screens/screen_02.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+export default Screen_02 = () => {
+    return (
+        <View>
+            <Text>
+                Hello
+            </Text>
+        </View>
+    )
+}
